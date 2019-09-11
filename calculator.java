4649455d479e8847ebb8aa3d730524648d906d6a
Shad Ahmed <shad.ahmed25@gmail.com>
2019-09-11 18:18:34 +0600

    addition Program added in this calculator

--- calculator.java
+++ calculator.java
@@ -1,5 +1,12 @@
 public class Calculator{
 	public static void main(String[] args){
 
+	System.out.println(addition(3,4));
+		
+	}
+
+	double addition(double x, int double){
+		return x + y;	
+
 	}
 }
