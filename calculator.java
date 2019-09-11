76d9f34ebcda5d42a22d1d96f4c3fb7595b1f3eb
Shad Ahmed <shad.ahmed25@gmail.com>
2019-09-11 18:24:08 +0600

    subtraction program added in this calculator

--- calculator.java
+++ calculator.java
@@ -2,11 +2,18 @@ public class Calculator{
 	public static void main(String[] args){
 
 	System.out.println(addition(3,4));
+	System.out.println(subtraction(13,4));	
 		
 	}
 
 	double addition(double x, int double){
-		return x + y;	
+		return x + y;
+
+	}
+	double subtraction(double x, double y){
+		
+		return x - y;
+		
 
 	}
 }
